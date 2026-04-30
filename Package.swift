e167c15045f5f2154f19deec3fa6ac12fbd2352c
RafaelXiola <10737197@mackenzista.com.br>
2026-04-30 18:30:48 +0000

    V1

--- Package.swift
+++ Package.swift
@@ -5,13 +5,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "TIC-PooSwift", // Aqui você deve alterar para o nome do seu repositorio
+    name: "PreparandoAmbiente", // Aqui você deve alterar para o nome do seu repositorio
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "Projeto_Academia"
-        ),        
+        ),
+        
+    ]
 )
 
 //para rodar cada aula, no terminal: swift run <nome da pasta da aula> - Exemplo: swift run Aula_1
