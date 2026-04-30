c8488a98ebbc40ed1e30523ddd7e2f9cb00b9cc4
RafaelXiola <10737197@mackenzista.com.br>
2026-04-30 15:19:07 -0300

    Rename package and update executable targets

--- Package.swift
+++ Package.swift
@@ -5,26 +5,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "PreparandoAmbiente", // Aqui você deve alterar para o nome do seu repositorio
+    name: "TIC-PooSwift", // Aqui você deve alterar para o nome do seu repositorio
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "Aula_1"
-        ),
-        .executableTarget(
-            name: "Aula_2"
-        ),
-        .executableTarget(
-            name: "Aula_3"
-        ),
-        .executableTarget(
-            name: "Aula_4"
-        ),
-        .executableTarget(
-            name: "Aula_5"
-        ),
-    ]
+            name: "Projeto_Academia"
+        ),        
 )
 
 //para rodar cada aula, no terminal: swift run <nome da pasta da aula> - Exemplo: swift run Aula_1
